50aa70129a644474563febef6f6b1fedcaa42fc3
Ilya_Zinkovich <Ilya_Zinkovich@epam.com>
2016-03-19 13:48:38 +0300

    Adding static content with node packages

---
+++ company-static/src/main/webapp/js/company-module.js
@@ -0,0 +1,54 @@
+angular.module('companyApp', [])
+	.controller('DepartmentController', ['DepartmentService', function(departmentService) {
+		var self = this;
+
+		self.departments = function() {
+			return departmentService.list();
+		}
+
+		self.create = function() {
+			departmentService.create({id : 4, name : self.department.name});
+		};
+		self.update = function(dept) {
+			dept.name = "lalala"
+		};
+	}])
+	.factory('DepartmentService', [function() {
+		var departments = [
+			{id : 1, name : 'Cobalt Maintenance'},
+			{id : 2, name : 'TRI-CPL'},
+			{id : 3, name : 'FS Maintenance'}
+		];
+		return {
+			list: function() {
+				return departments;
+			},
+			create: function(department) {
+				departments.push(department);
+			}
+		};
+	}])
+	//.factory('ProjectService', ['$resource', function($resource) {
+	//	return $resource('/api/project/:id');
+	//}]);
+
+
+/*
+OO-style of service definition
+
+function DepartmentService() {
+	var departments = [
+		{id : 1, name : 'Cobalt Maintenance'},
+		{id : 2, name : 'TRI-CPL'},
+		{id : 3, name : 'FS Maintenance'}
+	];
+	this.list = function() {
+		return departments;
+	};
+	this.create = function(department) {
+		departments.push(department);
+	};
+}
+
+Provider may be uses to define a service
+*/
